239092a72f29151a60d71a04c91050e4a982d81b
Christopher Lizon <Christopher.Lizon@gmail.com>
2019-07-10 17:05:07 -0500

    Shapes showing with cards

--- js/app2.js
+++ js/app2.js
@@ -6,33 +6,40 @@ const game = {
 	flippedCard1: "",
 	flippedCard2: "",
 
-	cardDeck: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
-	shapeValue: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+	cardDeck: [],
+	// shapeValue: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
 	arrayofCards: [],
 
 	generateCards() {
 
-		let card1 = 0;
-		let card2 = 0;
+		// let card1 = 0;
+		// let card2 = 0;
 
 
-		card1 = Math.ceil(Math.random() * this.cardDeck.length);
-		this.cardDeck.splice(card1, 1);
-		card2 = Math.ceil(Math.random() * this.cardDeck.length);
-		this.cardDeck.splice(card2, 1);
+		// card1 = Math.ceil(Math.random() * this.cardDeck.length);
+		// this.cardDeck.splice(card1, 1);
+		// card2 = Math.ceil(Math.random() * this.cardDeck.length);
+		// this.cardDeck.splice(card2, 1);
 
-		shape = Math.floor(Math.random() * this.shapeValue.length);
-		this.shapeValue.splice(shape, 1);
-
-		this.arrayofCards.push(new Card(card1, arrayOfShapes[shape], arrayOfShapeTags[shape]));
-		this.arrayofCards.push(new Card(card2, arrayOfShapes[shape], arrayOfShapeTags[shape]));
+		// shape = Math.floor(Math.random() * this.shapeValue.length);
+		// this.shapeValue.splice(shape, 1);
 
 		console.log(this.arrayofCards);
+
+		// loop up to 15
+
+			this.arrayofCards.push(new Card(i, arrayOfShapes[i], arrayOfShapeTags[i]));
+			this.arrayofCards.push(new Card(i, arrayOfShapes[i], arrayOfShapeTags[i]));
+
+		// randomize -- fisher yates
+
 		this.displayCards();
 
 	},
 	displayCards(){
 
+		// loop over cards
+			// card.print()
 		for(let i = 1; i <= 30; i++){
 			
 			// create 2
---
+++ js/card2.js
@@ -0,0 +1,15 @@
+class Card {
+	constructor(name, shape, shapeTag) {
+		this.name = name;
+		this.shape = shape;
+		this.shapeTag = shapeTag;
+	}
+
+	flip() {
+
+	}
+
+	print() {
+		// return a div built with jquery
+	}
+}
