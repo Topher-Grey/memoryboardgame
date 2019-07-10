4566001c356eb8ff76c0ff31d00f8c4de9562fe2
Christopher Lizon <Christopher.Lizon@gmail.com>
2019-07-10 13:40:05 -0500

    Shapes created and array added

--- js/app.js
+++ js/app.js
@@ -1,50 +1,33 @@
-for (let i = 1; i <= 6; i++){
-	for(let j = 1; j <= 6; j++){
-		const $div = $('<span></span>').attr('id', `${i}${j}`).addClass('playingcards');
-		// $div.text(`${i}${j}`)
-		$("#parent_container").append($div); 
+for (let i = 1; i <= 30; i++){
+	
+	const $div = $('<span></span>').attr('id', `${i}`).addClass('playingcards');
+	$("#parent_container").append($div); 
 
-	}
 }
 
 const flippedSquares = [];
 let stringNumbers = "";
 
-// $('span').mouseover(function () {
-// 	$(this).css({'background-color': 'midnightblue',
-// 		'background-image': 'url(images/GreenCircle.png)'})
-// 	})
 
-$('span').mouseenter(function () {
-	$(this).css({'background-color': 'midnightblue',
+$('span').on("click", function (e) {
+	$(e.currentTarget).css({'background-color': 'midnightblue',
 		'background-image': 'url(images/GreenCircle.png)'})
 
-	console.log(this.id);
-	flippedSquares.push(this.id);
+	console.log(e.currentTarget.id);
 
-	console.log(flippedSquares);
-	})
-
-// function roundTimer () {
-// 	const timer = setInterval(() => {
-
-// 		this.time++;
-
-//         if(this.time === 120){
-//           clearInterval(timer);
+})
 
 
 
-          
-//         }
+const game = {
 
-//       $('#timer').text(`Round Time: ${this.time}s`);
-
-// 	}, 1000);
-// }
+	// Generate Variables for game
+	let flippedCard1 = "",
+	let flippedCard2 = "",
 
 
-// time = 0;
 
 
-// roundTimer();
+}
+
+
---
+++ js/shapes.js
@@ -0,0 +1,37 @@
+arrayOfShapes = [
+	'url(images/GreenCircle.png',
+	'url(images/GreenSquare.png',
+	'url(images/GreenTriangle.png',
+	'url(images/GreenDiamond.png',
+	'url(images/RedCircle.png',
+	'url(images/RedSquare.png',
+	'url(images/RedTriangle.png',
+	'url(images/RedDiamond.png',
+	'url(images/BlueCircle.png',
+	'url(images/BlueSquare.png',
+	'url(images/BlueTriangle.png',
+	'url(images/BlueDiamond.png',
+	'url(images/OrangeCircle.png',
+	'url(images/OrangeSquare.png',
+	'url(images/OrangeTriangle.png',
+	'url(images/OrangeDiamond.png'
+];
+
+arrayOfShapeTags = [
+	'Green_Circle',
+	'Green_Square',
+	'Green_Triangle',
+	'Green_Diamond',
+	'Red_Circle',
+	'Red_Square',
+	'Red_Triangle',
+	'Red_Diamond'
+	'Blue_Circle',
+	'Blue_Square',
+	'Blue_Triangle',
+	'Blue_Diamond'
+	'Orange_Circle',
+	'Orange_Square',
+	'Orange_Triangle',
+	'Orange_Diamond'
+]
