23ce2600ea6034db59ac7887733076e8db3fe8c2
Christopher Lizon <Christopher.Lizon@gmail.com>
2019-07-10 10:53:18 -0500

    ID value can be captured during mouse event

--- js/app.js
+++ js/app.js
@@ -1,13 +1,50 @@
 for (let i = 1; i <= 6; i++){
 	for(let j = 1; j <= 6; j++){
-		const $div = $('<span></span>').attr('id', `#square${i}${j}`).addClass('playingcards');
+		const $div = $('<span></span>').attr('id', `${i}${j}`).addClass('playingcards');
+		// $div.text(`${i}${j}`)
 		$("#parent_container").append($div); 
 
 	}
 }
 
-$('span').mouseover(function () {
+const flippedSquares = [];
+let stringNumbers = "";
+
+// $('span').mouseover(function () {
+// 	$(this).css({'background-color': 'midnightblue',
+// 		'background-image': 'url(images/GreenCircle.png)'})
+// 	})
+
+$('span').mouseenter(function () {
 	$(this).css({'background-color': 'midnightblue',
 		'background-image': 'url(images/GreenCircle.png)'})
+
+	console.log(this.id);
+	flippedSquares.push(this.id);
+
+	console.log(flippedSquares);
 	})
 
+// function roundTimer () {
+// 	const timer = setInterval(() => {
+
+// 		this.time++;
+
+//         if(this.time === 120){
+//           clearInterval(timer);
+
+
+
+          
+//         }
+
+//       $('#timer').text(`Round Time: ${this.time}s`);
+
+// 	}, 1000);
+// }
+
+
+// time = 0;
+
+
+// roundTimer();
