e15d51959050fc00fc6d4c97803fe48b0115a1d3
Christopher Lizon <Christopher.Lizon@gmail.com>
2019-07-11 16:57:51 -0500

    Timer and scoring functioning

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ const game = {
 	selectingCard1: true,
 	selectingCard2: false,
 	score: 0,
+	remainingPairs: 15,
+	time: 120,
+	scoremultiplier: 10,
 
 	cardDeck: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
 	shapeValue: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
@@ -27,12 +30,12 @@ const game = {
 			index1 = Math.floor(Math.random() * this.cardDeck.length);
 			card1 = this.cardDeck[index1];
 			this.cardDeck.splice(index1, 1);
-			this.arrayofCards.push(new Card(card1 + 1, arrayOfShapes[shape], arrayOfShapeTags[shape]));
+			this.arrayofCards.push(new Card(card1 + 1, arrayOfShapes[shape], arrayOfShapeTags[shape], true));
 
 			index2 = Math.floor(Math.random() * this.cardDeck.length);
 			card2 = this.cardDeck[index2]
 			this.cardDeck.splice(index2, 1);
-			this.arrayofCards.push(new Card(card2 + 1, arrayOfShapes[shape], arrayOfShapeTags[shape]));
+			this.arrayofCards.push(new Card(card2 + 1, arrayOfShapes[shape], arrayOfShapeTags[shape], true));
 
 		}
 
@@ -41,8 +44,10 @@ const game = {
 		
 		this.displayShapes();
 		setTimeout(() => {
-			this.hideShapes()
-		}, 2000);
+			this.hideShapes();
+			this.setTimer();
+		}, 4000);
+
 
 
 
@@ -83,12 +88,36 @@ const game = {
 	},
 	updateScore(change, direction) {
 		if(direction == 'add'){
-			this.score += change;
+			this.score += change * this.scoremultiplier;
 		}
 		if(direction == 'subtract'){
 			this.score -= change;
 		}
+		if(this.score < 0){
+			this.score = 0;
+		}
 		$('#score').text(`Score: ${this.score}`);
+	},
+	displayWin(){
+
+	},
+	setTimer(){
+		const timer = setInterval(() => {
+        	this.time --;
+        	if(this.time === 0){
+          		clearInterval(timer);
+        	}
+      		$('#timer').text(`Time: ${this.time}s`);
+
+      		if(this.time % 3 == 0){
+      			this.scoremultiplier--;
+      		}
+
+      		if(this.scoremultiplier <= 0){
+      			this.scoremultiplier = 1;
+      		}
+
+    	}, 1000);
 	}
 
 }
@@ -102,7 +131,7 @@ $('#parent_container').on("click", function (e) {
 	if(game.selectingCard2){
 		console.log("Card2");
 		for(let i = 0; i < game.arrayofCards.length; i++){
-			if((e.target.id) == game.arrayofCards[i].name){
+			if(((e.target.id) == game.arrayofCards[i].name) && game.arrayofCards[i].cardOn){
 				cssString = game.arrayofCards[i].shape
 				game.flippedCard2 = i;
 				game.selectingCard2 = false;	
@@ -113,7 +142,7 @@ $('#parent_container').on("click", function (e) {
 	if(game.selectingCard1){
 		console.log("Card1");
 		for(let i = 0; i < game.arrayofCards.length; i++){
-			if((e.target.id) == game.arrayofCards[i].name){
+			if(((e.target.id) == game.arrayofCards[i].name) && game.arrayofCards[i].cardOn){
 				cssString = game.arrayofCards[i].shape
 				game.flippedCard1 = i;
 				game.selectingCard1 = false;
@@ -128,24 +157,38 @@ $('#parent_container').on("click", function (e) {
 	})
 	
 	if(!game.selectingCard1 && !game.selectingCard2){
+		console.log("Match");
 		game.selectingCard1 = true;
 		if(game.arrayofCards[game.flippedCard1].shapeTag == game.arrayofCards[game.flippedCard2].shapeTag){
+			game.arrayofCards[game.flippedCard1].cardOn = false;
+			game.arrayofCards[game.flippedCard2].cardOn = false;
 			game.updateScore(10, 'add');
-			console.log("Match Found!");
+			game.remainingPairs--;
+			console.log(game.remainingPairs);
+			if(game.remainingPairs == 0){
+				game.displayWin();
+			}
 		} else {
 			setTimeout(() => {
-				$(`#${game.arrayofCards[game.flippedCard1].name}`).css({
-				'background': 'rgb(161,10,10)',
-				'background': '-moz-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
-				'background': '-webkit-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
-				'background': 'linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)'
-				})
-				$(`#${game.arrayofCards[game.flippedCard2].name}`).css({
-				'background': 'rgb(161,10,10)',
-				'background': '-moz-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
-				'background': '-webkit-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
-				'background': 'linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)'
-				})
+				if(game.arrayofCards[game.flippedCard1].cardOn){
+					console.log("Card 1 flip");
+					$(`#${game.arrayofCards[game.flippedCard1].name}`).css({
+					'background': 'rgb(161,10,10)',
+					'background': '-moz-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
+					'background': '-webkit-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
+					'background': 'linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)'
+					})
+				};
+				if(game.arrayofCards[game.flippedCard2].cardOn){
+					console.log("Card 2 flip");
+					$(`#${game.arrayofCards[game.flippedCard2].name}`).css({
+					'background': 'rgb(161,10,10)',
+					'background': '-moz-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
+					'background': '-webkit-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
+					'background': 'linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)'
+					})
+				};
+				
 				game.updateScore(1, 'subtract');
 			}, 500);
 			
--- js/card.js
+++ js/card.js
@@ -1,15 +1,9 @@
 class Card {
-	constructor(name, shape, shapeTag) {
+	constructor(name, shape, shapeTag, cardOn) {
 		this.name = name;
 		this.shape = shape;
 		this.shapeTag = shapeTag;
+		this.cardOn = cardOn;
 	}
 
-	flip() {
-
-	}
-
-	print() {
-		
-	}
 }
