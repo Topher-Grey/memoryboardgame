d2c5b60fc7250e4d4525071d8e0fce2f768468c8
Christopher Lizon <Christopher.Lizon@gmail.com>
2019-07-11 10:24:53 -0500

    Game board displaying cards completed

--- js/app.js
+++ js/app.js
@@ -6,64 +6,97 @@ const game = {
 	flippedCard1: "",
 	flippedCard2: "",
 
-	cardDeck: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+	cardDeck: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
 	shapeValue: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
 	arrayofCards: [],
 
 	generateCards() {
 
-		let card1 = 0;
-		let card2 = 0;
+		let card1 = null;
+		let card2 = null;
 
+		for(let x = 1; x <= 15; x++){
 
-		card1 = Math.ceil(Math.random() * this.cardDeck.length);
-		this.cardDeck.splice(card1, 1);
-		card2 = Math.ceil(Math.random() * this.cardDeck.length);
-		this.cardDeck.splice(card2, 1);
+			indexS = Math.floor(Math.random() * this.shapeValue.length);
+			shape = this.shapeValue[indexS]
+			this.shapeValue.splice(indexS, 1);
 
-		shape = Math.floor(Math.random() * this.shapeValue.length);
-		this.shapeValue.splice(shape, 1);
+			index1 = Math.floor(Math.random() * this.cardDeck.length);
+			card1 = this.cardDeck[index1];
+			this.cardDeck.splice(index1, 1);
+			this.arrayofCards.push(new Card(card1 + 1, arrayOfShapes[shape], arrayOfShapeTags[shape]));
 
-		this.arrayofCards.push(new Card(card1, arrayOfShapes[shape], arrayOfShapeTags[shape]));
-		this.arrayofCards.push(new Card(card2, arrayOfShapes[shape], arrayOfShapeTags[shape]));
+			index2 = Math.floor(Math.random() * this.cardDeck.length);
+			card2 = this.cardDeck[index2]
+			this.cardDeck.splice(index2, 1);
+			this.arrayofCards.push(new Card(card2 + 1, arrayOfShapes[shape], arrayOfShapeTags[shape]));
 
-		console.log(this.arrayofCards);
+		}
+
 		this.displayCards();
 
+		// ***************** TIMER NEEDED AT GAME START *******************
+		this.displayShapes();
+		this.hideShapes();
+
+		
+
 	},
-	displayCards(){
+	displayCards() {
 
 		for(let i = 1; i <= 30; i++){
-			
-			// create 2
-			// push into array
-			// randomize array
-			// move this logic into game.printCards()
 
 			const $div = $('<span></span>').attr('id', `${i}`).addClass('playingcards');
 			$("#parent_container").append($div); 
 		}
+	},
+	displayShapes() {
+
+		let cssString = "";
+
+		for(let s = 1; s <= 30; s++){
+			for(let c = 0; c < this.arrayofCards.length; c++){
+				if(this.arrayofCards[c].name == s){
+					cssString = this.arrayofCards[c].shape
+				}
+				$(`#${s}`).css({
+					'background-color': 'midnightblue',
+					'background-image': cssString
+				});
+			}	
+		}
+	},
+	hideShapes() {
+		for(let c = 1; c <= 30; c++){
+			$(`#${c}`).css({
+				'background': 'rgb(161,10,10)',
+				'background': '-moz-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
+				'background': '-webkit-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
+				'background': 'linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)'
+			});
+		}
 	}
+
 }
 
 $('#parent_container').on("click", function (e) {
-	console.log(e.currentTarget);
-	console.log(e.target);
+	// console.log(e.currentTarget);
+	// console.log(e.target);
 	let cssString = "";
-	for(let i = 0; i < game.arrayofCards; i++){
-		console.log(game.arrayofCards[i].name);
+	for(let i = 0; i < game.arrayofCards.length; i++){
+		// console.log(game.arrayofCards[i].name);
 
 		if((e.target.id) == game.arrayofCards[i].name){
 			cssString = game.arrayofCards[i].shape
 		}
 	}
-	console.log(cssString);
-	$(e.target.id).css({
+	// console.log(cssString);
+	$(e.target).css({
 		'background-color': 'midnightblue',
 		'background-image': cssString
 	})
 
-	console.log(e.target.id);
+	// console.log(e.target.id);
 
 })
 
--- js/shapes.js
+++ js/shapes.js
@@ -1,20 +1,20 @@
 arrayOfShapes = [
-	'url(images/GreenCircle.png',
-	'url(images/GreenSquare.png',
-	'url(images/GreenTriangle.png',
-	'url(images/GreenDiamond.png',
-	'url(images/RedCircle.png',
-	'url(images/RedSquare.png',
-	'url(images/RedTriangle.png',
-	'url(images/RedDiamond.png',
-	'url(images/BlueCircle.png',
-	'url(images/BlueSquare.png',
-	'url(images/BlueTriangle.png',
-	'url(images/BlueDiamond.png',
-	'url(images/OrangeCircle.png',
-	'url(images/OrangeSquare.png',
-	'url(images/OrangeTriangle.png',
-	'url(images/OrangeDiamond.png'
+	'url(images/GreenCircle.png)',
+	'url(images/GreenSquare.png)',
+	'url(images/GreenTriangle.png)',
+	'url(images/GreenDiamond.png)',
+	'url(images/RedCircle.png)',
+	'url(images/RedSquare.png)',
+	'url(images/RedTriangle.png)',
+	'url(images/RedDiamond.png)',
+	'url(images/BlueCircle.png)',
+	'url(images/BlueSquare.png)',
+	'url(images/BlueTriangle.png)',
+	'url(images/BlueDiamond.png)',
+	'url(images/OrangeCircle.png)',
+	'url(images/OrangeSquare.png)',
+	'url(images/OrangeTriangle.png)',
+	'url(images/OrangeDiamond.png)'
 ];
 
 arrayOfShapeTags = [
