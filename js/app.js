9f548f03cad976a297915706fc22ab194f69afe1
Christopher Lizon <Christopher.Lizon@gmail.com>
2019-07-11 11:30:28 -0500

    Card matching functioning

--- js/app.js
+++ js/app.js
@@ -3,8 +3,11 @@
 const game = {
 
 	// Generate Variables for game
-	flippedCard1: "",
-	flippedCard2: "",
+	flippedCard1: 0,
+	flippedCard2: 0,
+	selectingCard1: true,
+	selectingCard2: false,
+	score: 0,
 
 	cardDeck: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
 	shapeValue: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
@@ -35,11 +38,13 @@ const game = {
 
 		this.displayCards();
 
-		// ***************** TIMER NEEDED AT GAME START *******************
-		this.displayShapes();
-		this.hideShapes();
-
 		
+		this.displayShapes();
+		setTimeout(() => {
+			this.hideShapes()
+		}, 2000);
+
+
 
 	},
 	displayCards() {
@@ -75,28 +80,79 @@ const game = {
 				'background': 'linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)'
 			});
 		}
+	},
+	updateScore(change, direction) {
+		if(direction == 'add'){
+			this.score += change;
+		}
+		if(direction == 'subtract'){
+			this.score -= change;
+		}
+		$('#score').text(`Score: ${this.score}`);
 	}
 
 }
 
 $('#parent_container').on("click", function (e) {
-	// console.log(e.currentTarget);
-	// console.log(e.target);
-	let cssString = "";
-	for(let i = 0; i < game.arrayofCards.length; i++){
-		// console.log(game.arrayofCards[i].name);
 
-		if((e.target.id) == game.arrayofCards[i].name){
-			cssString = game.arrayofCards[i].shape
+	let cssString = "";
+
+	
+
+	if(game.selectingCard2){
+		console.log("Card2");
+		for(let i = 0; i < game.arrayofCards.length; i++){
+			if((e.target.id) == game.arrayofCards[i].name){
+				cssString = game.arrayofCards[i].shape
+				game.flippedCard2 = i;
+				game.selectingCard2 = false;	
+			}
 		}
 	}
-	// console.log(cssString);
+
+	if(game.selectingCard1){
+		console.log("Card1");
+		for(let i = 0; i < game.arrayofCards.length; i++){
+			if((e.target.id) == game.arrayofCards[i].name){
+				cssString = game.arrayofCards[i].shape
+				game.flippedCard1 = i;
+				game.selectingCard1 = false;
+				game.selectingCard2 = true;	
+			}
+		}
+	}
+	
 	$(e.target).css({
 		'background-color': 'midnightblue',
 		'background-image': cssString
 	})
+	
+	if(!game.selectingCard1 && !game.selectingCard2){
+		game.selectingCard1 = true;
+		if(game.arrayofCards[game.flippedCard1].shapeTag == game.arrayofCards[game.flippedCard2].shapeTag){
+			game.updateScore(10, 'add');
+			console.log("Match Found!");
+		} else {
+			setTimeout(() => {
+				$(`#${game.arrayofCards[game.flippedCard1].name}`).css({
+				'background': 'rgb(161,10,10)',
+				'background': '-moz-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
+				'background': '-webkit-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
+				'background': 'linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)'
+				})
+				$(`#${game.arrayofCards[game.flippedCard2].name}`).css({
+				'background': 'rgb(161,10,10)',
+				'background': '-moz-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
+				'background': '-webkit-linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)',
+				'background': 'linear-gradient(180deg, rgba(161,10,10,1) 0%, rgba(252,70,107,1) 61%)'
+				})
+				game.updateScore(1, 'subtract');
+			}, 500);
+			
+		}
 
-	// console.log(e.target.id);
+	}
+
 
 })
 
